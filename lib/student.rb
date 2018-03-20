b11b62693e9445ec28967367e31e2c9b14ee5f66
Sam. O <samrey2018@gmail.com>
2018-03-20 14:11:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student < InteractiveRecord
                 DB[:conn].execute(sql)
         end
 
-        def self.find_by
+        def self.find_by(row)
                 sql = "SELECT * FROM #{self.row} WHERE ? = ?"
                 DB[:conn].execute(sql)
         end
